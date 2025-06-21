10fd2ee2526359009640f95297018b432b72b79e
Wil Wade <wil.wade@projectliberty.io>
2025-06-21 12:17:05 -0400

    Add basic test

---
+++ src/index.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import { startSiwf } from "./index.js";
+
+describe("Basic startSiwf test", () => {
+  it("Can do a thing", async () => {
+    const resp = await startSiwf(
+      "0xabcd",
+      () => "0xdef0",
+      () => ({ ok: true, json: () => ({}) }),
+      "",
+      "handle-here",
+      "email@example.com",
+      () => {},
+    );
+    expect(resp).toBeDefined();
+  });
+});
