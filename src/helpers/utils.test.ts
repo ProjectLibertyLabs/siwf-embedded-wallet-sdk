a577689c00d59de422e903fe868e1d7e51175e1d
Julian Fortune <julian.fortune@projectliberty.io>
2025-06-25 14:23:29 -0700

    Implements `convertSS58AddressToEthereum`

---
+++ src/helpers/utils.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+import { convertSS58AddressToEthereum } from "./utils";
+
+
+describe("convertSS58AddressToEthereum", () => {
+  it("succeeds", async () => {
+    const controlKey = "f6d1YDa4agkaQ5Kqq8ZKwCf2Ew8UFz9ot2JNrBwHsFkhdtHEn"
+    const input = { controlKey: controlKey, foo: "bar"}
+
+    const result = convertSS58AddressToEthereum(input)
+
+    const expected = "0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac"
+    expect(result).toStrictEqual({ controlKey: expected, foo: "bar"})
+  });
+});
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,4 +1,7 @@
 import { HexString } from "@frequency-chain/ethereum-utils";
+import { u8aToHex } from "@polkadot/util";
+import { addressToEvm } from "@polkadot/util-crypto"
+import { ethers } from "ethers"
 
 /**
  * Validate that a string is a valid hex string
@@ -8,4 +11,16 @@ export function isHexString(value: string): value is HexString {
   const hexRegex = /^0[xX][0-9a-fA-F]*$/;
   const isHex = hexRegex.test(value);
   return isHex && value.length % 2 === 0;
+}
+
+// Con
+export function convertSS58AddressToEthereum<T extends { controlKey: string }>(
+  input: T,
+): T {
+  const ethereumAddress = ethers.getAddress(u8aToHex(addressToEvm(input.controlKey)))
+
+  return {
+    ...input,
+    controlKey: ethereumAddress,
+  };
 }
